193d4a1a549fb55bf34f0d87ccecd18c1d2ae450
Taeho Gwon <taehogwon96@gmail.com>
2022-03-10 11:12:38 +0900

    Add counts in hand

--- src/schema/hand.py
+++ src/schema/hand.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import Counter
 from typing import Optional
 
 from pydantic import BaseModel
@@ -15,11 +16,11 @@ class Hand(BaseModel):
     draw_tile: Optional[Tile]
 
     @property
-    def is_opened(self):
+    def is_opened(self) -> bool:
         return any(call.type != CallType.CONCEALED_KAN for call in self.calls)
 
     @property
-    def tiles(self):
+    def tiles(self) -> list[Tile]:
         ret = self.concealed_tiles[:]
 
         for call in self.calls:
@@ -29,3 +30,7 @@ class Hand(BaseModel):
             ret.append(self.draw_tile)
 
         return ret
+
+    @property
+    def counts(self) -> dict[Tile, int]:
+        return Counter(self.tiles)
