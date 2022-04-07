02b922d3da3c7eb1b56128ca786af57453cd122b
Taeho Gwon <taehogwon96@gmail.com>
2022-04-07 09:43:00 +0900

    add normal agari test

--- src/tests/test_agari_checker.py
+++ src/tests/test_agari_checker.py
@@ -33,3 +33,18 @@ def test_check_agari_seven_pair(test_input, expected):
 def test_check_agari_thirteen_orphans(test_input, expected):
     hand = get_hand_from_code(test_input)
     assert check_agari_thirteen_orphans(hand) == expected
+
+
+@pytest.mark.skip
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("123s456p99m,chi123s,chi12-3s", True),
+        ("123s456p99m,cok111-1z,cok3-333z", True),
+        ("1133s4455p99m1177z", False),
+        ("19m19s19p12334567z", False),
+    ],
+)
+def test_check_agari_normal(test_input, expected):
+    hand = get_hand_from_code(test_input)
+    assert check_agari_thirteen_orphans(hand) == expected
