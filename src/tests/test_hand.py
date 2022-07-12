1103003183d7d696bde1af5b75bf4a7b8474908b
Taeho Gwon <taehogwon96@gmail.com>
2022-07-12 22:55:42 +0900

    test hand iter concealed tiles

--- src/tests/test_hand.py
+++ src/tests/test_hand.py
@@ -32,6 +32,61 @@ def test_hand_is_opened(test_input, expected):
                 Tile(type=TileType.PIN, value=5),
                 Tile(type=TileType.PIN, value=6),
                 Tile(type=TileType.MAN, value=9),
+                Tile(type=TileType.MAN, value=9),
+            ],
+        ),
+        (
+            "123s123456789p99m",
+            [
+                Tile(type=TileType.SOU, value=1),
+                Tile(type=TileType.SOU, value=2),
+                Tile(type=TileType.SOU, value=3),
+                Tile(type=TileType.PIN, value=1),
+                Tile(type=TileType.PIN, value=2),
+                Tile(type=TileType.PIN, value=3),
+                Tile(type=TileType.PIN, value=4),
+                Tile(type=TileType.PIN, value=5),
+                Tile(type=TileType.PIN, value=6),
+                Tile(type=TileType.PIN, value=7),
+                Tile(type=TileType.PIN, value=8),
+                Tile(type=TileType.PIN, value=9),
+                Tile(type=TileType.MAN, value=9),
+                Tile(type=TileType.MAN, value=9),
+            ],
+        ),
+        (
+            "123s456p99m,cok111-1z,cok3-333z",
+            [
+                Tile(type=TileType.SOU, value=1),
+                Tile(type=TileType.SOU, value=2),
+                Tile(type=TileType.SOU, value=3),
+                Tile(type=TileType.PIN, value=4),
+                Tile(type=TileType.PIN, value=5),
+                Tile(type=TileType.PIN, value=6),
+                Tile(type=TileType.MAN, value=9),
+                Tile(type=TileType.MAN, value=9),
+            ],
+        ),
+    ],
+)
+def test_hand_iter_concealed_tiles(test_input, expected):
+    hand = get_hand_from_code(test_input)
+    assert list(hand.iter_concealed_tiles) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (
+            "123s456p99m,chi123s,chi12-3s",
+            [
+                Tile(type=TileType.SOU, value=1),
+                Tile(type=TileType.SOU, value=2),
+                Tile(type=TileType.SOU, value=3),
+                Tile(type=TileType.PIN, value=4),
+                Tile(type=TileType.PIN, value=5),
+                Tile(type=TileType.PIN, value=6),
+                Tile(type=TileType.MAN, value=9),
                 Tile(type=TileType.SOU, value=1),
                 Tile(type=TileType.SOU, value=2),
                 Tile(type=TileType.SOU, value=3),
