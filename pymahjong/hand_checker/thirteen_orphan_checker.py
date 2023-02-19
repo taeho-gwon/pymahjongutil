4bd3967388a3b014fdc8fd726d2bc80e91efe4a7
Taeho Gwon <taehogwon96@gmail.com>
2023-02-19 23:23:51 +0900

    add thirteen orphan checker

---
+++ pymahjong/hand_checker/thirteen_orphan_checker.py
@@ -0,0 +1,17 @@
+from mahjong.shanten import Shanten
+
+from pymahjong.hand_checker.hand_checker import HandChecker
+from pymahjong.schema.count import HandCount
+
+
+class ThirteenOrphanChecker(HandChecker):
+    def __init__(self):
+        self.shanten_calculator = Shanten()
+
+    def calculate_deficiency(self, hand_count: HandCount) -> int:
+        return (
+            self.shanten_calculator.calculate_shanten_for_kokushi_hand(
+                hand_count.concealed_count.convert_to_list34()
+            )
+            + 1
+        )
