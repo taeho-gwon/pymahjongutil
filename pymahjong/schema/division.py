dccfdee518733a3ae1f1a2f8b5fde97417bd5332
Taeho Gwon <taehogwon96@gmail.com>
2023-05-11 19:31:01 +0900

    add normal divider

--- pymahjong/schema/division.py
+++ pymahjong/schema/division.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
-from pymahjong.enum.common import DivisionPartTypeEnum
+from pymahjong.enum.common import CallTypeEnum, DivisionPartTypeEnum
+from pymahjong.schema.call import Call
 from pymahjong.schema.count import TileCount
 from pymahjong.schema.tile import Tile, Tiles
 
@@ -11,10 +12,26 @@ class DivisionPart(BaseModel):
     is_concealed: bool
 
     @staticmethod
-    def create_head(head_tile: Tile, is_concealed: bool):
+    def create_head(tile: Tile, is_concealed: bool):
         return DivisionPart(
             type=DivisionPartTypeEnum.HEAD,
-            counts=TileCount.create_from_tiles([head_tile] * 2),
+            counts=TileCount.create_from_tiles([tile] * 2),
+            is_concealed=is_concealed,
+        )
+
+    @staticmethod
+    def create_triple(tile: Tile, is_concealed: bool):
+        return DivisionPart(
+            type=DivisionPartTypeEnum.TRIPLE,
+            counts=TileCount.create_from_tiles([tile] * 3),
+            is_concealed=is_concealed,
+        )
+
+    @staticmethod
+    def create_straight(tile: Tile, is_concealed: bool):
+        return DivisionPart(
+            type=DivisionPartTypeEnum.STRAIGHT,
+            counts=TileCount.create_from_tiles([tile, tile.next, tile.next.next]),
             is_concealed=is_concealed,
         )
 
@@ -26,6 +43,22 @@ class DivisionPart(BaseModel):
                 Tiles.TERMINALS_AND_HONORS + [head_tile]
             ),
             is_concealed=is_concealed,
+        )
+
+    @staticmethod
+    def create_from_call(call: Call):
+        match call.type:
+            case CallTypeEnum.CHII:
+                part_type = DivisionPartTypeEnum.STRAIGHT
+            case CallTypeEnum.PON:
+                part_type = DivisionPartTypeEnum.TRIPLE
+            case _:
+                part_type = DivisionPartTypeEnum.QUAD
+
+        return DivisionPart(
+            type=part_type,
+            counts=TileCount.create_from_tiles(call.tiles),
+            is_concealed=call.type is CallTypeEnum.CONCEALED_KAN,
         )
 
 
