ac2af0b39c58e12ad1b9e4eb6d702d831a609eba
Taeho Gwon <taehogwon96@gmail.com>
2023-05-23 21:52:49 +0900

    add chiihou test

--- tests/pymahjong/yaku_checker/utils.py
+++ tests/pymahjong/yaku_checker/utils.py
@@ -3,10 +3,13 @@ from pymahjong.hand_parser import get_hand_from_code
 from pymahjong.schema.agari_info import AgariInfo
 
 
-def assert_yaku_check(test_input, expected, yaku):
+def assert_yaku_check(test_input, expected, yaku, agari_info=None):
+    if agari_info is None:
+        agari_info = AgariInfo()
+
     hand = get_hand_from_code(test_input)
     result = any(
-        yaku.is_satisfied(division, AgariInfo())
+        yaku.is_satisfied(division, agari_info)
         for division in RiichiChecker(hand).calculate_divisions(True)
     )
     assert result == expected
---
+++ tests/pymahjong/yaku_checker/yakuman/test_chiihou.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pymahjong.schema.agari_info import AgariInfo
+from pymahjong.schema.tile import Tiles
+from pymahjong.yaku_checker.yakuman.chiihou import Chiihou
+from tests.pymahjong.yaku_checker.utils import assert_yaku_check
+
+
+@pytest.mark.parametrize(
+    "expected, is_tsumo_agari, player_wind, is_first_turn",
+    [
+        (True, True, Tiles.WINDS[1], True),
+        (False, True, Tiles.WINDS[0], True),
+        (False, True, Tiles.WINDS[1], False),
+        (False, False, Tiles.WINDS[1], True),
+    ],
+)
+def test_chiihou(expected, is_tsumo_agari, player_wind, is_first_turn):
+    agari_info = AgariInfo(
+        is_tsumo_agari=is_tsumo_agari,
+        player_wind=player_wind,
+        is_first_turn=is_first_turn,
+    )
+    assert_yaku_check("123456789m11199p", expected, Chiihou(), agari_info)
