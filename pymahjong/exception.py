686213dbc5a4863a7ce6622c5d9b68d882deeca7
Taeho Gwon <taehogwon96@gmail.com>
2023-05-23 16:54:18 +0900

    fix error

--- pymahjong/exception.py
+++
@@ -1,6 +0,0 @@
-class TileInputError(Exception):
-    def __init__(self, tile_code: str):
-        self.tile_code = tile_code
-
-    def __str__(self):
-        return self.tile_code + "is not a valid tile_code"
