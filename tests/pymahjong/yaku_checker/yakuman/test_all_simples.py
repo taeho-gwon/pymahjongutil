0ea17714e78511ddea06ddf233ccd6a56f341e87
Taeho Gwon <taehogwon96@gmail.com>
2023-06-01 19:26:36 +0900

    add test for all simples

---
+++ tests/pymahjong/yaku_checker/yakuman/test_all_simples.py
@@ -0,0 +1,17 @@
+import pytest
+
+from pymahjong.yaku_checker.all_simples import AllSimples
+from tests.pymahjong.yaku_checker.utils import assert_yaku_check
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("34m445566p67888s2m", True),
+        ("66777888m678p77s6m", True),
+        ("13m456p22345678s2m", False),
+        ("2333m444p555s666z2m", False),
+    ],
+)
+def test_all_simples(test_input, expected):
+    assert_yaku_check(test_input, expected, AllSimples())
