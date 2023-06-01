384e6fdacd29f3bad7e93d9c75ca25080de0dbbb
Taeho Gwon <taehogwon96@gmail.com>
2023-06-01 19:34:49 +0900

    add test for all terminals and honors

--- pymahjong/yaku_checker/all_terminals_and_honors.py
+++ pymahjong/yaku_checker/all_terminals_and_honors.py
@@ -1,6 +1,7 @@
 from pymahjong.enum.common import YakuEnum
 from pymahjong.schema.agari_info import AgariInfo
 from pymahjong.schema.division import Division
+from pymahjong.schema.tile import Tiles
 from pymahjong.yaku_checker.base_yaku import BaseYaku
 
 
@@ -9,4 +10,4 @@ class AllTerminalsAndHonors(BaseYaku):
         super().__init__(YakuEnum.ALL_TERMINALS_AND_HONORS)
 
     def is_satisfied(self, division: Division, agari_info: AgariInfo):
-        raise NotImplementedError
+        return division.tile_count.is_containing_only(Tiles.TERMINALS_AND_HONORS)
