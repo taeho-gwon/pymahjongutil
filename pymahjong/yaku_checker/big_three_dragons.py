a77c758b9c9faa8839e92e9f24cf4d1342926247
Taeho Gwon <taehogwon96@gmail.com>
2023-06-03 16:56:01 +0900

    add big three dragons

--- pymahjong/yaku_checker/big_three_dragons.py
+++ pymahjong/yaku_checker/big_three_dragons.py
@@ -1,6 +1,7 @@
-from pymahjong.enum.common import YakuEnum
+from pymahjong.enum.common import DivisionPartTypeEnum, YakuEnum
 from pymahjong.schema.agari_info import AgariInfo
 from pymahjong.schema.division import Division
+from pymahjong.schema.tile import Tiles
 from pymahjong.yaku_checker.base_yaku import BaseYaku
 
 
@@ -9,4 +10,13 @@ class BigThreeDragons(BaseYaku):
         super().__init__(YakuEnum.BIG_THREE_DRAGONS)
 
     def is_satisfied(self, division: Division, agari_info: AgariInfo):
-        raise NotImplementedError
+        num_dragon_triplets = sum(
+            1
+            for part in division.parts
+            if part.counts.is_containing_only(Tiles.DRAGONS)
+            and (
+                part.type is DivisionPartTypeEnum.TRIPLE
+                or part.type is DivisionPartTypeEnum.QUAD
+            )
+        )
+        return num_dragon_triplets == 3
