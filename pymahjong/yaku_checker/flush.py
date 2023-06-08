6cb68b1be717f4b2d685c9b976785d9e95a6d525
Taeho Gwon <taehogwon96@gmail.com>
2023-06-08 14:47:39 +0900

    add flush

--- pymahjong/yaku_checker/flush.py
+++ pymahjong/yaku_checker/flush.py
@@ -1,6 +1,7 @@
 from pymahjong.enum.common import YakuEnum
 from pymahjong.schema.agari_info import AgariInfo
 from pymahjong.schema.division import Division
+from pymahjong.schema.tile import Tiles
 from pymahjong.yaku_checker.base_yaku import BaseYaku
 
 
@@ -9,4 +10,9 @@ class Flush(BaseYaku):
         super().__init__(YakuEnum.FLUSH)
 
     def is_satisfied(self, division: Division, agari_info: AgariInfo):
-        raise NotImplementedError
+        tile_count = division.tile_count
+        return (
+            tile_count.is_containing_only(Tiles.MANS)
+            or tile_count.is_containing_only(Tiles.PINS)
+            or tile_count.is_containing_only(Tiles.SOUS)
+        )
