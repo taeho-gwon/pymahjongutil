13bab11bdef2f3c397044f4d2793d2c9552b9ea3
Taeho Gwon <taehogwon96@gmail.com>
2023-06-08 17:52:50 +0900

    add two sets of identical sequences

--- pymahjong/yaku_checker/two_sets_of_identical_sequences.py
+++ pymahjong/yaku_checker/two_sets_of_identical_sequences.py
@@ -1,4 +1,6 @@
-from pymahjong.enum.common import YakuEnum
+from itertools import combinations
+
+from pymahjong.enum.common import DivisionPartTypeEnum, YakuEnum
 from pymahjong.schema.agari_info import AgariInfo
 from pymahjong.schema.division import Division
 from pymahjong.yaku_checker.base_yaku import BaseYaku
@@ -9,4 +11,10 @@ class TwoSetsOfIdenticalSequences(BaseYaku):
         super().__init__(YakuEnum.TWO_SETS_OF_IDENTICAL_SEQUENCES)
 
     def is_satisfied(self, division: Division, agari_info: AgariInfo):
-        raise NotImplementedError
+        num_identical_sequences = sum(
+            1
+            for part1, part2 in combinations(division.parts, 2)
+            if part1.type is part2.type is DivisionPartTypeEnum.STRAIGHT
+            and part1.counts == part2.counts
+        )
+        return num_identical_sequences == 2 or num_identical_sequences == 6
