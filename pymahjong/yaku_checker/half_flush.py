51ab869e71fd928eb43e135d3818905dfeedf39f
Taeho Gwon <taehogwon96@gmail.com>
2023-06-08 15:36:55 +0900

    add half flush

--- pymahjong/yaku_checker/half_flush.py
+++ pymahjong/yaku_checker/half_flush.py
@@ -1,6 +1,7 @@
 from pymahjong.enum.common import YakuEnum
 from pymahjong.schema.agari_info import AgariInfo
 from pymahjong.schema.division import Division
+from pymahjong.schema.tile import Tiles
 from pymahjong.yaku_checker.base_yaku import BaseYaku
 
 
@@ -9,4 +10,9 @@ class HalfFlush(BaseYaku):
         super().__init__(YakuEnum.HALF_FLUSH)
 
     def is_satisfied(self, division: Division, agari_info: AgariInfo):
-        raise NotImplementedError
+        tile_count = division.tile_count
+        return (
+            tile_count.is_containing_only(Tiles.MANS + Tiles.HONORS)
+            or tile_count.is_containing_only(Tiles.PINS + Tiles.HONORS)
+            or tile_count.is_containing_only(Tiles.SOUS + Tiles.HONORS)
+        )
---
+++ tests/pymahjong/yaku_checker/test_half_flush.py
@@ -0,0 +1,20 @@
+import pytest
+
+from pymahjong.schema.agari_info import AgariInfo
+from pymahjong.yaku_checker.half_flush import HalfFlush
+from tests.pymahjong.yaku_checker.utils import assert_yaku_check
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("11123456789999s", True),
+        ("33345699m,pon111m,smk2222m", True),
+        ("22334455667788p", True),
+        ("223344556677p11z", True),
+        ("33345699m,pon111z,smk2222m", True),
+        ("33345699m,pon111z,smk2222p", False),
+    ],
+)
+def test_half_flush(test_input, expected):
+    assert_yaku_check(test_input, expected, HalfFlush(), AgariInfo())
